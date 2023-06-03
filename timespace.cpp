01d5064ebe9d01c2c98fec4bd7e6ab7f70ad425f
Shaaz Mukadam <84740787+Shaaz-Mukadam@users.noreply.github.com>
2023-06-03 17:56:29 +0530

    Add files via upload

---
+++ timespace.cpp
@@ -0,0 +1,17 @@
+//*********************TIME COMPLEXITY*****************
+//Time complexity means how much time is required to tun the program.
+//OR
+//Time complexity of an algorithm qurantifies the amout of the time taken by a program to run as function of length of input.
+
+int n ;
+cin>>n;
+int a = 0;
+for(int i=1; i<n;i++){
+    a=a+1;
+}
+
+n=5
+i = 1 , 2  ,3 , 4
+linear propotional to n
+
+follow from notebook
